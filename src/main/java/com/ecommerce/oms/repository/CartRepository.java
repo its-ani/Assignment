5e9ce36641afe39e2b798ad82cfcea1461bda4ce
its-ani <guptaanirudh712@gmail.com>
2026-07-13 23:14:19 +0530

    repository: cart query methods

--- src/main/java/com/ecommerce/oms/repository/CartRepository.java
+++ src/main/java/com/ecommerce/oms/repository/CartRepository.java
@@ -1,10 +1,13 @@
 package com.ecommerce.oms.repository;
 
 import com.ecommerce.oms.domain.Cart;
+import com.ecommerce.oms.domain.CartStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findByCustomerIdAndStatus(UUID customerId, CartStatus status);
 }
